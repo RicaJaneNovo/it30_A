36aaaf4569b31723ef3d6f149e6f4691705bb7a9
RicaJaneNovo <ricajanenovo88@gmail.com>
2026-08-27 19:57:00 +0800

    LAB4_08/27/2026

--- backups/lab3.sql
+++ backups/lab3.sql
@@ -11,38 +11,86 @@ INSERT INTO books (book_title,book_author,book_category) VALUES ("Project Hail M
 
 
 
-CREATE TABLE borrow(
-       borrow_id INT PRIMARY KEY AUTO_INCREMENT,
+CREATE TABLE borrows (
+    borrow_id INT NOT NULL AUTO_INCREMENT,
+    student_id INT NOT NULL,
+    book_id INT NOT NULL,
 
-       student_id INT NOT NULL,
-       book_id INT NOT NULL,
+    borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    borrow_return_date TIMESTAMP NULL DEFAULT NULL,
 
-       borrow_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-       borrow_return_date TIMESTAMP NULL DEFAULT NULL,
-       
-       CONSTRAINT fk_borrow_student FOREIGN KEY (student_id) 
-       REFERENCES student(student_id), 
+    PRIMARY KEY (borrow_id),
 
-       CONSTRAINT fk_borrow_book FOREIGN KEY (book_id) 
-       REFERENCES books(book_id)
+    CONSTRAINT fk_borrow_student
+        FOREIGN KEY (student_id)
+        REFERENCES students(student_id),
+
+    CONSTRAINT fk_borrow_book
+        FOREIGN KEY (book_id)
+        REFERENCES books(book_id)
+
 );
 
-INSERT INTO borrow (student_id,book_id) VALUES (1,2), (2,1), (3,3);
 
-SELECT 
-     br.borrow_id,
+--August 27,2026
 
-     s.student_id,
-     CONCAT(s.student_first_name, ' ',s.student_last_name) as student_name, s.student_course,
+SELECT
+    br.borrow_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    s.student_course,
+    b.book_title,
+    b.book_author,
+    b.book_category,
+    br.borrow_date,
+    br.borrow_return_date
+FROM borrows br
+JOIN students s
+    ON br.student_id = s.student_id
+JOIN books b
+    ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NULL
+ORDER BY br.borrow_date DESC;
 
-     b.book_title,
-     b.book_author,
-     b.book_category
 
-     br.borrow_date
-FROM borrow br
-JOIN student s ON br.student_id = s.student_id
-JOIN book b ON br.book_id = b.book_id
 
-WHERE br.borrow_return_date IS NULL,
+INSERT INTO borrows (student_id,book_id) VALUES (1,2), (2,1), (3,3);
+
+SELECT
+    br.borrow_id,
+    s.student_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    s.student_course,
+    b.book_title,
+    b.book_author,
+    b.book_category,
+    br.borrow_date
+FROM borrows br
+JOIN students s ON br.student_id = s.student_id
+JOIN books b ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NULL
+ORDER BY br.borrow_date DESC;
+
+UPDATE borrows
+SET borrow_return_date = CURRENT_TIMESTAMP
+WHERE borrow_id = 1
+AND borrow_return_date IS NULL;
+
+
+
+
+SELECT
+    br.borrow_id,
+    CONCAT(s.student_first_name, ' ', s.student_last_name) AS student_name,
+    s.student_course,
+    b.book_title,
+    b.book_author,
+    b.book_category,
+    br.borrow_date,
+    br.borrow_return_date
+FROM borrows br
+JOIN students s
+    ON br.student_id = s.student_id
+JOIN books b
+    ON br.book_id = b.book_id
+WHERE br.borrow_return_date IS NOT NULL
 ORDER BY br.borrow_date DESC;
